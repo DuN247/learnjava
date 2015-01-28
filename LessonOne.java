364218f8d84a822b8539037a5e67c92d07b128fa
niral mehta <niral@learnsofteng.com>
2015-01-28 15:18:14 +0000

    for loop implemenation

--- LessonOne.java
+++ LessonOne.java
@@ -2,6 +2,10 @@
 
 class LessonOne {
     public static void main(String[] args) {
-        System.out.println("new program"); 
+
+      for (int i = 0; i > 7; i+=2 ) {
+        System.out.println("Impending Doom!!!!!!");
+      }
+        System.out.println("new program");
     }
 }
