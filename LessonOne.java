38504b57a7379ed36e54efbf116d9097b64ec607
Rodrigo Queiroz <rodrigo@queiroz.org>
2015-01-28 15:21:10 +0000

    fixed issue with loop

--- LessonOne.java
+++ LessonOne.java
@@ -3,7 +3,7 @@
 class LessonOne {
     public static void main(String[] args) {
 
-      for (int i = 0; i > 7; i+=2 ) {
+      for (int i = 0; i < 7; i+=2 ) {
         System.out.println("Impending Doom!!!!!!");
       }
         System.out.println("new program");
